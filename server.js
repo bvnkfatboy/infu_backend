0b2c65dcfa2f19f7af5a466b2cb0b050a047fa39
Theerapong Bank <theerapong.bvnk@gmail.com>
2024-04-11 16:24:49 +0700

    update auth reg

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const socketio = require('socket.io');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// Controllers
+const authController = require('./controllers/auth/authController');
+
+mongoose.connect('mongodb+srv://admin:1234@infudb.njec3fj.mongodb.net/?retryWrites=true&w=majority&appName=InfuDB',{
+  useNewUrlParser: true
+})
 
 dotenv.config();
 
@@ -12,12 +18,16 @@ const server = require('http').Server(app);
 const io = socketio(server);
 
 app.use(cors());
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
 
 // ตั้งค่า route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
+app.post('/register', authController.registerUser);
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => {
